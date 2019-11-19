3d8d82d8555dcc4cb67a98511c3f5ee6d89cc197
alex <alex@kutusha.com>
2019-11-18 17:27:02 -0800

    Added more comments, readme and examples.

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# gv2graphson
-Convert GraphViz DOT/GV file to GraphSON
+[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)
+
+# GV to GraphSON converter
+
+This is a simple utility that converts GraphViz GV file to TinkerPop GraphSON format. It can convert visual attributes to node/edge labels. I use it with [Qt Visual Graph Editor](https://github.com/ArsMasiuk/qvge), which has export to GV.
+
+Utility converts node labels to property "name". If node has no label utility generates warning and uses original NodeId as "name".
+
+## Installation and usage
+
+* Install Python (tested with Python 3 only)
+* pip install pydot
+* Download [gv2graphson.py](https://raw.githubusercontent.com/sanychsamara/gv2graphson/master/gv2graphson.py) from this repository
+* Run "python.exe gv2graphson.py -f test_graph.gv"
+
+Full list of options
+
+
+```plaintext
+gv2graphson.py [-h] -f FILENAME [-n NODELABEL] [-e EDGELABEL] [-c EDGECOLOR] [-t EDGETHICKNESS] [-s EDGESTYLE] [-p NODESHAPE]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FILENAME, --filename FILENAME
+                        Input filename in GraphViz DV format
+  -n NODELABEL, --nodelabel NODELABEL
+                        Node label. Defaults to 'node'.
+  -e EDGELABEL, --edgelabel EDGELABEL
+                        Edge label. Defaults to 'edge'.
+  -c EDGECOLOR, --edgecolor EDGECOLOR
+                        Edge color to label mapping. Example: #cc9900=parent
+  -t EDGETHICKNESS, --edgethickness EDGETHICKNESS
+                        Edge thickness to label mapping. Example: 2=parent
+  -s EDGESTYLE, --edgestyle EDGESTYLE
+                        Edge style to label mapping. Example: dotted=parent
+  -p NODESHAPE, --nodeshape NODESHAPE
+                        Node shape to label mapping. Example: rect=person
+```
+
+
+## Example
+
+Let's say you have following graph built in Qt Visual Graph Editor. You used color "brown" to specify relationship "part_of" and color "green" for relationship "location". You also used round nodes for "logical containers" and square nodes for "physical containers".
+
+![](test_graph.png)
+
+This is the command you would run. Note that "ellipse" is a default visual attribute and not saved with each node. Therefore instead of "-p ellipse=physical" you would need to override default node label as "-n logical".
+
+```plaintext
+gv2graphson.py -f test_graph.gv -c #cc9900=part_of -c #00cc00=location -n logical -p rect=physical
+```
+
+Output file "test_graph.json" gets created in the same folder as input file and has extension "json". To verify conversion launch Gremlin Console and try following.
+
+```java
+Graph graph = TinkerGraph.open();
+graph.io(IoCore.graphson()).readGraph("test_graph.json");
+
+g = graph.traversal()
+g.V().union(label(),values('name'))
+g.E().label()
+g.V().has("name", "Order_N001").in().repeat(out()).until(hasLabel("physical")).values("name")
+
+// Last command output should look like
+==>Package_2
+==>Package_1
+==>Shelf_JBK9
+```
+
+## Related Projects
+
+There are 2 other similar converters on GitHub. One is written on Perl and one on Haskel. 
+Neither of them offers attributes-to-label mappings.
+
+https://metacpan.org/pod/distribution/Graph-Easy/bin/graph-easy
+https://github.com/portnov/dot2graphml/blob/master/dot2graphml.hs
--- gv2graphson.py
+++ gv2graphson.py
@@ -1,11 +1,23 @@
+#  
+# Copyright (c) Alexander Uslontsev. All rights reserved.  
+# Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+#
+
 import sys
 import os
 import json
+import argparse
 import pydot
 
-default_node_label = "concept"
-default_edge_label = "parent"
+# Default node label. Define here or use -n command line parameter.
+default_node_label = "node"
 
+# Default edge label. Define here or use -e command line parameter.
+default_edge_label = "edge"
+
+# Map edge color to edge label. 
+# Define here or use -c command line parameter.
+# List contains frequently used colors in Qt Visual Graph Editor
 edge_color_to_label_mapping = {
     # light brown
     "#cc9900": "",
@@ -26,21 +38,27 @@ edge_color_to_label_mapping = {
     "#ff3300": ""
 }
 
+# Map edge thickness to edge label
+# Define here or use -t command line parameter.
 edge_thickness_to_label_mapping = {
     "1": "",
     "2": "",
-    "4": "next",
+    "4": "",
 }
 
+# Map edge drawing style to edge label
+# Define here or use -s command line parameter.
 edge_style_to_label_mapping = {
     "solid": "",
     "dotted": "",
     "dashed": ""
 }
 
+# Map node shape to node label
+# Define here or use -p command line parameter.
 node_shape_to_label_mapping = {
-    "rect": "sequence",
-    "hexagon": "english",
+    "rect": "",
+    "hexagon": "",
     "ellipse": "",
 }
 
@@ -52,8 +70,6 @@ def main(gv_filename):
 
     pydot_graph_container = pydot.graph_from_dot_file(gv_filename)
 
-    graphson_nodes = {}
-
     # Internal structure of the graph returned by pydot library is weird
     # Graph has multiple dictionary-based container objects with 1 element inside
     # Graph also uses "obj_dict" intermediate dictionary for it's properties
@@ -64,16 +80,30 @@ def main(gv_filename):
     pydot_node_ids = pydot_graph.obj_dict["nodes"].keys()
     pydot_edge_ids = pydot_graph.obj_dict["edges"].keys()
 
+    # When converting edges in second loop we will have access to GV NodeIDs only (arbitrary strings)
+    # but to create a proper GraphSON edge we would need to know a newly created GraphSON NodeId (sequential integers)
+    # This dictionary is filled in the first loop when converting edges
+
     pydot_to_graphson_id_mappings = dict()
 
+    # --------------------------------
     # Converting nodes
+    # --------------------------------
 
+    graphson_nodes = {}    
     graphson_id = 0
     property_value_id = 0
 
+    total_nodes_converted = 0
+    total_edges_converted = 0
+
     for pydot_node_id in pydot_node_ids:
         
-        if pydot_node_id == "node":
+        # Pydot graph contains two nodes called 'node' and 'edge'
+        # that define default visual attributes for nodes and edges
+        # They are not part of the graph structure and should be ignored
+
+        if pydot_node_id == "node" or pydot_node_id == "edge":
             continue
 
         pydot_to_graphson_id_mappings[pydot_node_id] = graphson_id
@@ -109,17 +139,19 @@ def main(gv_filename):
         graphson_nodes[graphson_id] = graphson_node
 
         graphson_id += 1
+        total_nodes_converted += 1
         
+    # --------------------------------
     # Converting edges
+    # --------------------------------
+
+    # In GV format edges are saved in a separate list (that's why there is a second loop)
+    # In GraphSON format edges only exist as part of the nodes 
 
     for pydot_edge_id in pydot_edge_ids:
 
-        if pydot_edge_id == "edge":
-            continue
-
         pydot_edge = pydot_edges[pydot_edge_id][0]
         pydot_edge_attributes = pydot_edge["attributes"]
-        graphson_edge_label = pydot_edge_attributes.get("xlabel")
         
         graphson_edge = {} 
         graphson_edge["id"] = graphson_id
@@ -129,11 +161,15 @@ def main(gv_filename):
         pydot_node_id_from = pydot_edge.get("points")[0]
         pydot_node_id_to = pydot_edge.get("points")[1]
 
+        # Need to find GraphSON newly generated integer IDs
+        # because internal dictionary of nodes 'graphson_nodes' uses GraphSON IDs
         graphson_id_from = pydot_to_graphson_id_mappings[pydot_node_id_from]
         graphson_id_to = pydot_to_graphson_id_mappings[pydot_node_id_to]
 
-        graphson_edge["inV"] = graphson_id_from
-        graphson_edge["outV"] = graphson_id_to
+        # Out == from
+        # In == to
+        graphson_edge["outV"] = graphson_id_from
+        graphson_edge["inV"] = graphson_id_to
 
         # from
         graphson_node_from = graphson_nodes[graphson_id_from]
@@ -158,17 +194,22 @@ def main(gv_filename):
         graphson_nodes[graphson_id_to] = graphson_node_to
 
         graphson_id += 1 
+        total_edges_converted += 1
 
     graphson_filename_without_extension = os.path.splitext(gv_filename)[0]
     graphson_filename = graphson_filename_without_extension + ".json"
 
+    print("{0} nodes converted. {1} edges converted. Saving output file to '{2}'".format(total_nodes_converted, total_edges_converted, graphson_filename))
+
     with open(graphson_filename, 'w') as graphson_file:
         for graphson_node in graphson_nodes.values():
-            #json.dump(graphson_node, graphson_file, indent=4)
             json.dump(graphson_node, graphson_file)
             graphson_file.write("\n")
 
 
+# Go through multiple override options and return final node label for GraphSON node
+# At the moment only node shape can override label. 
+# Color and style overrides for nodes are not supported (I do not use them at the moment - feel free to add)
 def get_graphson_node_label(pydot_node_attributes):
 
     shape = pydot_node_attributes.get("shape")
@@ -180,10 +221,11 @@ def get_graphson_node_label(pydot_node_attributes):
 
     return default_node_label
 
-
+# Go through multiple override options and return final edge label for GraphSON edge
 def get_graphson_edge_label(pydot_edge_attributes):
     
-    color = pydot_edge_attributes.get("color")
+    # "Color" attribute name is double quoted in my GV files
+    color = pydot_edge_attributes.get("\"color\"")
     if color:
         color = color.strip('"')
         edge_label = edge_color_to_label_mapping.get(color)
@@ -206,15 +248,45 @@ def get_graphson_edge_label(pydot_edge_attributes):
 
     return default_edge_label
 
+# Adds or replaces "visual_attribute to label" mappings in one of the 4 default dictionary mappings
+#   edge_color     to edge_label
+#   edge_thickness to edge_label
+#   edge_style     to edge_label
+#   node_shape     to node_label
+
+def add_override_mapping_from_params(default_mappings_dict, name_values_from_args):
+    
+    if not name_values_from_args:
+        return
+    
+    params_mappings_dict = dict(name_value.split('=') for name_value in name_values_from_args)
+    for key in params_mappings_dict.keys():
+        default_mappings_dict[str(key)] = params_mappings_dict[key]
+    
 
 if __name__ == "__main__":
 
-    gv_filename = ""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename", help="Input filename in GraphViz DV format", required=True)
+    parser.add_argument("-n", "--nodelabel", help="Node label. Defaults to 'node'.", default="node")
+    parser.add_argument("-e", "--edgelabel", help="Edge label. Defaults to 'edge'.", default="edge")
 
-    if len(sys.argv) > 1:
-        gv_filename = sys.argv[1]
+    parser.add_argument("-c", "--edgecolor", help="Edge color to label mapping. Example: #cc9900=parent", action="append")
+    parser.add_argument("-t", "--edgethickness", help="Edge thickness to label mapping. Example: 2=parent", action="append")
+    parser.add_argument("-s", "--edgestyle", help="Edge style to label mapping. Example: dotted=parent", action="append")
+    parser.add_argument("-p", "--nodeshape", help="Node shape to label mapping. Example: rect=person", action="append")
 
-    if not gv_filename:
-        gv_filename = "test_graph.gv"
+    # read arguments from the command line
+    args = parser.parse_args()
 
-    main(gv_filename)    
+    default_node_label = args.nodelabel
+    default_edge_label = args.edgelabel
+
+    # Override visual_attribute-to-label mappings via command line params
+    add_override_mapping_from_params(edge_color_to_label_mapping, args.edgecolor)
+    add_override_mapping_from_params(edge_thickness_to_label_mapping, args.edgethickness)
+    add_override_mapping_from_params(edge_style_to_label_mapping, args.edgestyle)
+    add_override_mapping_from_params(node_shape_to_label_mapping, args.nodeshape)
+
+
+    main(args.filename)    
